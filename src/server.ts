e6b95b702d55f90b588253841c9099305dfd59dc
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-01-16 12:01:00 +0100

    cors

--- src/server.ts
+++ src/server.ts
@@ -11,12 +11,11 @@ import "./utils/response-handler";
 const app: Application = express();
 const port: number = +(process.env.PORT || 5000);
 
-// app.use(cors());
-app.options("*", cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(routes);
+app.use("/api" + routes);
 app.use(errorHandler);
 
 /**
