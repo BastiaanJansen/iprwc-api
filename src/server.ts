776ca3111aa319438ae93b0d2d26c78448657607
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-01-21 16:49:58 +0100

    rate limiter

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ const app: Application = express();
 const port: number = +(process.env.PORT || 5000);
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 5 * 60 * 1000, // 5 minutes
     max: 100, // limit each IP to 100 requests per windowMs
 });
 
